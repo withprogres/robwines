674d75c85fc7f0b816784e92784f956d054fcdff
Samuel Akins <akinyemisamuel170@gmail.com>
2023-08-31 16:15:26 +0100

    setup contact mail

---
+++ contact/index.php
@@ -0,0 +1,68 @@
+<?php
+require '../../config/mail.php';
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+$firstName = $_POST["firstName"];
+$lastName = $_POST["lastName"];
+$name = $firstName . ' ' . $lastName;
+
+$from = $_POST["email"];
+
+$address = $_POST["address"];
+$phone = $_POST["phone"];
+$car = $_POST["car"];
+$years = $_POST["years"];
+$bank = $_POST["bank"];
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_OFF;
+    $mail->isSMTP();
+    $mail->Host = SMTP_HOST;
+    $mail->SMTPAuth = true;
+    $mail->Username = SMTP_USERNAME;
+    $mail->Password = SMTP_PASSWORD;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
+    $mail->Port = SMTP_PORT; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->AddReplyTo($from, $name);
+    $mail->setFrom(SMTP_USERNAME, $name);
+    $mail->addAddress('akinyemisamuel170@gmail.com', 'Samuel Akins'); //Add a recipient
+    // $mail->addAddress('madgesiversonhjk29@gmail.com', 'Robert Winery'); //Add a recipient
+
+    //Content
+    $mail->isHTML(true); //Set email format to HTML
+    $mail->Subject = "ROBERT WINERY PORTFOLIO";
+    $mail->Body = "<h5>Contact by $name</h5>
+                    <p> email: $from<br>
+                    address: $address<br>
+                    phone: $phone<br>
+                    car: $car<br>
+                    years: $years<br>
+                    bank: $bank
+                    </p>";
+    $mail->AltBody = " Contact by $name ,
+                    email: $from,
+                    address: $address,
+                    phone: $phone,
+                    car: $car,
+                    years: $years,
+                    bank: $bank
+                    ";
+
+    $mail->send();
+    echo 'OK';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+?>
